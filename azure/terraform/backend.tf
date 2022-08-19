64abe6120cad5c36a7cd0314635426ef38b7a040
Jackie Tung <jackie@outerbounds.co>
2022-08-19 11:03:50 -0700

    remove azure/terraform/backend.tf

--- azure/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tf-state"
-    storage_account_name = "obtfstate"
-    container_name       = "ob-metaflow-on-azure"
-    key                  = "terraform.tfstate"
-  }
-}
