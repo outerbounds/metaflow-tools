360b085951e7de49b3c99b482e4ec5be74fcc4a7
Alexander <Salikov57@gmail.com>
2024-12-06 12:53:30 +0000

    Update providers.tf

--- nebius/terraform/providers.tf
+++ nebius/terraform/providers.tf
@@ -1,7 +1,3 @@
 provider "nebius" {
-  service_account = {
-    private_key_file = "~/.nebius/authkey/private.pem"
-    public_key_id    = "publickey-e00b751npdk0wsh86b"
-    account_id       = "serviceaccount-e00bvk1m7aetbsq7k6"
-  }
+  domain = "api.eu.nebius.cloud:443"
 }
